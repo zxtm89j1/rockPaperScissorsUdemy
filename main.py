c5c1088399bc3ad1a07a218c0b9b00aee72aa8fe
Spencer Bohol <106042655+zxtm89j1@users.noreply.github.com>
2023-08-22 22:21:54 +0800

    Rock paper scissors

---
+++ main.py
@@ -0,0 +1,47 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+#Write your code below this line 👇
+import random
+shapes = [rock, paper, scissors]
+
+choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper and 2 for Scissors."))
+print(shapes[choice])
+
+computer_choice = random.randint(0, 2)
+
+print("Computer choice")
+print(shapes[computer_choice])
+
+
+if choice == computer_choice:
+  print("It's a tie! You both choose the same shape!")
+elif (choice == 0 and computer_choice == 1) or (choice == 1 and computer_choice == 2) or (choice == 2 and computer_choice == 0):
+  print("Computer won!")
+else:
+  print("You won!")
+
