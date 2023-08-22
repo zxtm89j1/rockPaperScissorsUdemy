ecdd72cb190a4e54968bc429bf871c0b91857fbd
Spencer Bohol <106042655+zxtm89j1@users.noreply.github.com>
2023-08-22 23:36:32 +0800

    Update main.py - moved the random module at the top to stop an awkward pause in the program

--- main.py
+++ main.py
@@ -28,18 +28,19 @@ scissors = '''
 #Write your code below this line 👇
 import random
 shapes = [rock, paper, scissors]
+computer_choice = random.randint(0, 2)
 
 choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper and 2 for Scissors."))
 print(shapes[choice])
 
-computer_choice = random.randint(0, 2)
+
 
 print("Computer choice")
 print(shapes[computer_choice])
 
 
 if choice == computer_choice:
-  print("It's a tie! You both choose the same shape!")
+  print("It's a tie! You both chose the same shape!")
 elif (choice == 0 and computer_choice == 1) or (choice == 1 and computer_choice == 2) or (choice == 2 and computer_choice == 0):
   print("Computer won!")
 else:
